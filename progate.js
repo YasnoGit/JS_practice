371d36e6a890e5ab05d066a437653b1212282580
EC2 Default User <132417648+YasnoGit@users.noreply.github.com>
2023-09-10 07:03:12 +0000

    変数

--- progate.js
+++ progate.js
@@ -1,2 +1,30 @@
-console.log('Hello World')
-console.log("Hello World")
+console.log('Hello World');
+console.log("Hello World");
+console.log("にゃんじゃわんこ");
+console.log(3);
+console.log(5 + 2);
+console.log("3 - 7");
+console.log(20 - 8);
+console.log(3 * 7);
+console.log(6 / 3);
+console.log(9 % 2);
+console.log("にゃんじゃ" + "わんこ");
+console.log("20" + "15");
+
+// 変数
+let name = "john";
+console.log(name);
+name = "Kate";
+console.log(name);
+let nam = "にゃんじゃわんこ";
+console.log(name);
+let number = 11;
+console.log(number);
+number = number + 3;
+number = number + 10;
+number += 10
+number /= 
+console.log(number);
+let length = 5;
+console.log(length);
+console.log(length * length * 3);
