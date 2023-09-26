325889672f6de0ed372c33ef4980853b5ac9bc99
EC2 Default User <132417648+YasnoGit@users.noreply.github.com>
2023-09-26 11:34:37 +0000

    繰り返し基礎

--- progate.js
+++ progate.js
@@ -77,7 +77,100 @@ console.log(length);
 console.log(length * length * 3);
 
 // 定数
+const name1 = "Johnn"
+const language = "フランス語"
+console.log(language);
+console.log(language + "を話せます");
+
+// テンプレートリテラル
+const name2 = "にゃんじゃわんこ"
+const age = 14;
+console.log('僕の名前は ${name2} です');
+console.log('今は ${age}歳です');
+
+
+
+// 繰り返し
+console.log('Hello World');
+console.log("Hello World");
+console.log("にゃんじゃわんこ");
+console.log(3);
+console.log(5 + 2);
+console.log("3 - 7");
+console.log(20 - 8);
+console.log(3 * 7);
+console.log(6 / 3);
+console.log(9 % 2);
+console.log("にゃんじゃ" + "わんこ");
+console.log("20" + "15");
+
+// 変数
+let name = "john";
+console.log(name);
+name = "Kate";
+console.log(name);
+let nam = "にゃんじゃわんこ";
+console.log(name);
+let number = 11;
+console.log(number);
+number = number + 3;
+number = number + 10;
+number += 10
+number /=
+console.log(number);
+let length = 5;
+console.log(length);
+console.log(length * length * 3);
+
+// 定数
 const name1 = "John"
+const language = "フランス語"
+console.log(language);
+console.log(language + "を話せます");
+
+// テンプレートリテラル
+const name2 = "にゃんじゃわんこ"
+const age = 14;
+console.log('僕の名前は ${name2} です');
+console.log('今は ${age}歳です');
+
+
+
+
+
+console.log('Hello World');
+console.log("Hello World");
+console.log("にゃんじゃわんこ");
+console.log(3);
+console.log(5 + 2);
+console.log("3 - 7");
+console.log(20 - 8);
+console.log(3 * 7);
+console.log(6 / 3);
+console.log(9 % 2);
+console.log("にゃんじゃ" + "わんこ");
+console.log("20" + "15");
+
+// 変数
+let name = "john";
+console.log(name);
+name = "Kate";
+console.log(name);
+let nam = "にゃんじゃわんこ";
+console.log(name);
+let number = 11;
+console.log(number);
+number = number + 3;
+number = number + 10;
+number += 10
+number /=
+console.log(number);
+let length = 5;
+console.log(length);
+console.log(length * length * 3);
+
+// 定数
+const name1 = "Johnn"
 const language = "フランス語"
 console.log(language);
 console.log(language + "を話せます");
